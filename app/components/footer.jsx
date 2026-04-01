7522d745c03c71c675fc629b0dc75d98782214c3
Zaina Nazimudeen Ahmed <zainaahmed003@gmail.com>
2026-04-01 17:54:14 +0530

    Remove star and fork buttons, add LinkedIn and GitHub links to footer

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -1,40 +1,36 @@
 // @flow strict
 import Link from 'next/link';
-import { CgGitFork } from "react-icons/cg";
-import { IoStar } from "react-icons/io5";
 
 function Footer() {
   return (
     <div className="relative border-t bg-[#0d1224] border-[#353951] text-white">
       <div className="mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] py-6 lg:py-10">
         <div className="flex justify-center -z-40">
-          <div className="absolute top-0 h-[1px] w-1/2  bg-gradient-to-r from-transparent via-violet-500 to-transparent"></div>
+          <div className="absolute top-0 h-[1px] w-1/2 bg-gradient-to-r from-transparent via-violet-500 to-transparent"></div>
         </div>
         <div className="flex flex-col md:flex-row items-center justify-between">
           <p className="text-sm">
-            © Developer Portfolio by <Link target="_blank" href="https://www.linkedin.com/in/zainanahmed" className="text-[#16f2b3]">Zaina Ahmed</Link>
+            © Developer Portfolio by <Link target="_blank" href="https://www.linkedin.com/in/zainanahmed" className="text-[#16f2b3]">Zaina Nazimudeen Ahmed</Link>
           </p>
           <div className="flex items-center gap-5">
             <Link
               target="_blank"
-              href="https://github.com/Zaina523445/ZeeProjects"
-              className="flex items-center gap-2 uppercase hover:text-[#16f2b3]"
+              href="https://www.linkedin.com/in/zainanahmed"
+              className="flex items-center gap-2 uppercase hover:text-[#16f2b3] text-sm"
             >
-              <IoStar />
-              <span>Star</span>
+              <span>LinkedIn</span>
             </Link>
             <Link
               target="_blank"
               href="https://github.com/Zaina523445/ZeeProjects"
-              className="flex items-center gap-2 uppercase hover:text-[#16f2b3]"
+              className="flex items-center gap-2 uppercase hover:text-[#16f2b3] text-sm"
             >
-              <CgGitFork />
-              <span>Fork</span>
+              <span>GitHub</span>
             </Link>
           </div>
         </div>
       </div>
-    </div >
+    </div>
   );
 };
 
